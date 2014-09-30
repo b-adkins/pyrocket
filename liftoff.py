19b1fb55f86b9528799b6905dbb9e1c57996eee1
Bea Adkins <plz.bea.there@gmail.com>
2014-09-30 12:49:08 -0700

    Changed wet/dry mass to calculated values. Stages now store structural, propellant, and payload masses instead.

--- liftoff.py
+++ liftoff.py
@@ -6,11 +6,10 @@ from flight import *
 from planet import *
 from stage import *
 
-T1_test_vehicle = Stage(7.95e3, 3.55e3, 200e3, 320, 0.2)
+T1_test_vehicle = Stage(2e3, 1.55e3, 4e3, 200e3, 320, 0.2)
 
-T2_stage_2 = Stage(5.24e3, 3.24e3, 200e3, 370, 0.2)
-# For now manually add stage 2 wet mass; payload is just a Stage pointer
-T2_test_vehicle = Stage(8.9e3 + 5.24e3, 2.9e3 + 5.24e3, 200e3, 340, 0.2, payload = T2_stage_2)
+T2_stage_2 = Stage(1.75e3, 1.49e3, 2e3, 200e3, 370, 0.2)
+T2_test_vehicle = Stage(2.25e3, 5.99e3, 6e3, 200e3, 340, 0.2, payload = T2_stage_2)
 
 def main():
     # rocket = T1_test_vehicle
--- stage.py
+++ stage.py
@@ -7,12 +7,15 @@ g0 = 9.81
 ##
 # Class representing an arbitrary stage of a vehicle.
 class Stage(object):
-    ##
+    ## Create a Stage object.
+    #
+    # To make a multi-stage vehicle, start with the final stage. For each successive stage, include the previous stage as payload.
     #
     # Vehicle parameters
-    # m_0          Wet mass [kg]
-    # m_f          Dry mass [kg]
-    # playload     Payload Stage or None (currently only a pointer - include stage mass in wet/dry mass).
+    # m_s          Structural mass [kg]. Total mass of engines, fuel tanks (only the tank), and struts.
+    # m_l          Payload mass [kg]. Next stage wet mass or total mass of science instruments, capsules, space systems.
+    # m_p          Propellant mass [kg].
+    # playload     Payload Stage or None (currently only a pointer - include stage wet mass in payload mass).
     #
     # Thruster parameters
     # T    Thrust [N]
@@ -20,9 +23,10 @@ class Stage(object):
     #
     # Aerodynamic parameters
     # C_d   Coefficient of drag
-    def __init__(self, m_0, m_f, T, Isp, C_d, payload = None):
-        self.m_0 = m_0
-        self.m_f = m_f 
+    def __init__(self, m_s, m_l, m_p, T, Isp, C_d, payload = None):
+        self.m_s = m_s
+        self.m_l = m_l
+        self.m_p = m_p
 
         self.T = T
         self.Isp = Isp
@@ -30,6 +34,47 @@ class Stage(object):
         self.C_d = C_d
 
         self.payload = payload
+
+    @property
+    def m_0(self):
+        """ Calculate wet mass."""
+        return self.m_f + self.m_p
+
+    @property
+    def m_f(self):
+        """ Calculate dry mass."""
+        return self.m_l + self.m_s
+
+    @property
+    def m_l(self):
+        """ Get payload mass. """
+        return self._m_l
+
+    @m_l.setter
+    def m_l(self, value):
+        """ Set payload mass. """
+        self._m_l = value
+
+    @property
+    def m_p(self):
+        """ Get propellant mass. """
+        return self._m_p
+
+    @m_p.setter
+    def m_p(self, value):
+        """ Set propellant mass. """
+        self._m_p = value
+
+    @property
+    def m_s(self):
+        """ Get structural mass. """
+        return self._m_s
+
+    @m_s.setter
+    def m_s(self, value):
+        """ Set structural mass. """
+        self._m_s = value
+
         
     ##
     # Calculates area for use in drag equations. Grossly oversimplified in KSP, based on mass.
